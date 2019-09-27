e3c459c415ec35c07056e474ffe03aa5c2304293
PabloYza <P.deyzaguirre13@gmail.com>
2019-09-27 12:02:30 +0200

    Rename

--- ExpressCRUD.js
+++ ExpressCRUD.js
@@ -36,23 +36,21 @@ app.get('/api/courses', (req, res) => {
 });
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send('The course was not found');
+  if (!course) return res.status(404).send('The course with the given ID was not found');
+
   res.send(course);
 });
 
 // (path/url, routeHandler(res, req))
 app.post('/api/courses', (req, res) => {
+  // Look up the course - if not existing, return 400
   const { error } = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   const course = {
     id: courses.length + 1,
     name: req.body.name
   };
-  console.log(course)
   courses.push(course);
   res.send(course);
 });
@@ -61,20 +59,31 @@ app.post('/api/courses', (req, res) => {
 app.put('/api/courses/:id', (req, res) => {
   // Look up the course - if not existing, return 404
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send('The course was not found');
+  if (!course) return res.status(404).send('The course with the given ID was not found');
 
   // Validate - If invalid, return 400 bad request
   const { error } = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  };
+  if (error) return res.status(400).send(error.details[0].message);
+
 
   //update course - return updated course
   course.name = req.body.name;
   res.send(course);
 });
 
+
+app.delete('/api/courses/:id', (req, res) => {
+  // Look up the course - if not existing, return 404
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('The course with the given ID was not found');
+
+  // Delete
+  const index = courses.indexOf(course);
+  courses.splice(index, 1); // we go to said index, and remove 1 OBJECT
+
+  // Return the same course
+  res.send(course);
+});
 
 
 
